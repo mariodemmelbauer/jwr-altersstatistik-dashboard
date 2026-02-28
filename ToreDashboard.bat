ab05bcb4950f22bf662c913503abe4cbc92a58a3
Demmelbauer Mario <mario.demmelbauer@coc-ag.de>
2026-02-28 11:39:48 +0100

    Aktualisierung Daten

--- ToreDashboard.bat
+++ ToreDashboard.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Starte Streamlit-Dashboard für Tore-Analysen
-set PYTHON="C:\Users\demmelb-ma\AppData\Local\Programs\Python\Python313\python.exe"
+set PYTHON="C:\Users\demmelb-ma\AppData\Local\Programs\Python\Python314\python.exe"
 set SCRIPT="C:\Users\demmelb-ma\OneDrive - COC AG\JWR\Analysen\2526\dashboard_fixed.py"
 
 %PYTHON% -m streamlit run %SCRIPT%
